ecbc20b6636b2b3a88d2d52bd0b52922f0b2402f
Koree <koreewilliam@gmail.com>
2014-03-11 22:33:31 -0400

    seeing if geolocation works

--- transit/static_coords.js
+++ transit/static_coords.js
@@ -1,31 +1,32 @@
 var myLat = 0;
 var myLng = 0;
 var lineData = null;
+var map;
 
 function initialize() {
 		var myOptions = {
 
-			center: new google.maps.LatLng(0,0),
+			//center: new google.maps.LatLng(0,0),
 			zoom: 11 };
 		// "..." is stuff you have to fill in
 
 		
 	
-		var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 		getMyLocation();
 		parse();
 	}
 
-function getMyLocation() 
-			{
+function getMyLocation() {
 				console.log ('hi');
 				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
 					navigator.geolocation.getCurrentPosition(function(position) {
 						myLat = position.coords.latitude;
 						myLng = position.coords.longitude;
-						rendermap();
 						console.log ('maybe');
 						console.log(myLng);
+						rendermap();
+						
 					});
 				}
 				else {
@@ -34,9 +35,9 @@ function getMyLocation()
 			}
 
 function rendermap() {
-me = new google.maps.LatLng(myLat, myLng)
-map.setCenter(me,12)
-zoom:11 	
+var me = new google.maps.LatLng(myLat, myLng);
+map.setCenter(me);
+zoom:11; 	
 }
 
 function parse() {
