da55e1fc72e11a614ba1684b67ac8f55d0db8191
Koree <koreewilliam@gmail.com>
2014-03-11 23:57:52 -0400

    seeing if geolocation works

--- transit/static_coords.js
+++ transit/static_coords.js
@@ -1,61 +1,66 @@
-var myLat = 0;
-var myLng = 0;
-/*var lineData = null;
-var me = new google.maps.LatLng(myLat, myLng)
-var map;
-
-var marker;
-var	places;// "..." is stuff you have to fill in
-*/
+var lat = 0;
+var lng = 0;
+var scheduleData = null;
+var myOptions = {
+                                        center: new google.maps.LatLng (lat , lng),
+                                        zoom: 11
+                                };
 var map;
 var request;
-var myOptions = {
 
-			center: new google.maps.LatLng(myLat, myLng),
-			zoom: 11 
-			//mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-function initialize() {
-		
-		map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-		getMyLocation();
-//		parse();
-	}
+red = new Object
+
+
+
+function initialize(){
+        map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        getMyLocation();
+        parse();
+}
 
 function getMyLocation() {
-				console.log ('hi');
-				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-					navigator.geolocation.getCurrentPosition(function(position) {
-						myLat = position.coords.latitude;
-						myLng = position.coords.longitude;
-						console.log ('nope');
-						console.log(myLat, myLng);
-						
-
-						renderMap();
-						
-					});
-					
-				}
-				else {
-					alert("Geolocation is not supported by your web browser.  What a shame!");
-				}
-			}
-
-function renderMap() {
- me = new google.maps.LatLng(myLat, myLng);
- 	map.setCenter(me,12);
-
+        if (navigator.geolocation)
+        {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                lat = position.coords.latitude;
+                lng = position.coords.longitude;
+                renderMap();
+                });
+    }
+    else
+    {
+            alert("Geolocation is not supported by your web browser.  What a shame!");
+        }
 
 
 }
 
-/*function parse() {
+function renderMap()
+{
+        me = new google.maps.LatLng(lat, lng);
+        map.setCenter(me, 12);
 
-	request = new XMLHttpRequest();
-	request.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
-	request.onreadystatechange = dataReady;
-	request.send(null);
 }
 
-	//lineData = JSON.parse(request.responseText)
+
+function parse()
+{
+        request = new XMLHttpRequest();
+        request.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+        request.onreadystatechange = dataReady;
+        request.send(null);
+
+}
+function dataReady() {
+        scheduleData =JSON.parse(request.responseText);
+        if(request.status == 200) {
+                scheduleData =JSON.parse(request.responseText);
+                console.log('yay');
+
+        }
+        else if (request.status == 500)
+        {
+                console.log('uh oh');
+        }
+
+}
