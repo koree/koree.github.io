949a9393311aeab7b4c47d94192b27b59ed3595e
Koree <koreewilliam@gmail.com>
2014-03-12 00:03:18 -0400

    seeing if geolocation works

--- transit/static_coords.js
+++ transit/static_coords.js
@@ -1,66 +1,17 @@
-var lat = 0;
-var lng = 0;
-var scheduleData = null;
-var myOptions = {
-                                        center: new google.maps.LatLng (lat , lng),
-                                        zoom: 11
-                                };
-var map;
-var request;
-
-red = new Object
-
-
-
-function initialize(){
-        map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-        getMyLocation();
-        parse();
-}
-
-function getMyLocation() {
-        if (navigator.geolocation)
-        {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                lat = position.coords.latitude;
-                lng = position.coords.longitude;
-                renderMap();
-                });
-    }
-    else
+function getLocation()
+  {
+  if (navigator.geolocation)
     {
-            alert("Geolocation is not supported by your web browser.  What a shame!");
-        }
-
-
-}
-
-function renderMap()
-{
-        me = new google.maps.LatLng(lat, lng);
-        map.setCenter(me, 12);
-
-}
-
-
-function parse()
-{
-        request = new XMLHttpRequest();
-        request.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
-        request.onreadystatechange = dataReady;
-        request.send(null);
-
-}
-function dataReady() {
-        scheduleData =JSON.parse(request.responseText);
-        if(request.status == 200) {
-                scheduleData =JSON.parse(request.responseText);
-                console.log('yay');
-
-        }
-        else if (request.status == 500)
-        {
-                console.log('uh oh');
-        }
-
-}
+    navigator.geolocation.getCurrentPosition(initialize);
+    }
+  else{alert("Geolocation is not supported by this browser.");}
+  }
+  function initialize(position) {
+        var mapOptions = {
+          center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+          zoom: 15
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+window.onload = loadScript;  
