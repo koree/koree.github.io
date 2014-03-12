ed676b8a5b25f4c90ebbc0566577d9517fff6657
Koree <koreewilliam@gmail.com>
2014-03-12 15:43:30 -0400

    trying with differenst stations

--- transit2/transit.js
+++ transit2/transit.js
@@ -59,10 +59,10 @@ mapinfo();
 var locations = [];
 
 function mapinfo(){
- console.log('come on already')
+
 for (var i =0; i < 3; ++i){
     if (parsed[i]["line"] == color){
-                   
+                    console.log('come on already')
         var array = i;
         var j = 0;
             for (var j = 0; parsed[i]["stations"][j] != null; ++j){
