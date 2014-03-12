3dd16b45b7a9ddbda1f15a94d9ba02a0e2963977
Koree <koreewilliam@gmail.com>
2014-03-12 18:27:09 -0400

    trying with differenst stations

--- transit2/transit.js
+++ transit2/transit.js
@@ -94,7 +94,7 @@ for (var i =0; i < 3; i++){
                 });
             }
             console.log(parsed[i]["stations"][j]["latitude"]);
-            console.log(marker.position["k"]);
+            console.log(marker.position.lat);
 
                 google.maps.event.addListener(m, 'click', function(){
                     bubble.setContent(m.title);
