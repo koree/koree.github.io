a5c5eac6aab4bedfd291292430202f39f464b154
Koree <koreewilliam@gmail.com>
2014-03-12 14:36:22 -0400

    new approach to geolocation

--- transit2/transit.js
+++ transit2/transit.js
@@ -1,3 +1,5 @@
+var map;
+var me;
 
 function getMyLocation() {
 				console.log ('hi');
@@ -9,31 +11,30 @@ function getMyLocation() {
 			}
 
 function initialize(position){
-    console.log('here?');
-var myLat= position.coords.latitude;
-var myLng= position.coords.longitude;
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    var myLat= position.coords.latitude;
+    var myLng= position.coords.longitude;
 
 
-var me= new google.maps.LatLng(myLat, myLng);
-var myOptions = {
-    center: me,
-    zoom:11
-};
+    me = new google.maps.LatLng(myLat, myLng);
+    var myOptions = {
+        center: me,
+        zoom:11
+    };
 
-var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-var marker = new google.maps.Marker({
-    position: me,
-    title: "Work damn you"
-});
-marker.setMap(map);
-bubble = new google.maps.InfoWindow();
-google.maps.event.addListener(marker, 'click', function(){
-    bubble.setContent(marker.title);
-    bubble.open(map, marker);
-});
+    var marker = new google.maps.Marker({
+        position: me,
+        title: "Work damn you"
+    });
+    marker.setMap(map);
+    bubble = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'click', function(){
+        bubble.setContent(marker.title);
+        bubble.open(map, marker);
+    });
 
-//parse(map);
-console.log(myLat); // program has gotten this far... still no map
+    //parse(map);
+    console.log(myLat, myLng); // program has gotten this far... still no map
 }
 
 
