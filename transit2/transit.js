ba23abc0ac6d0ea001bda8265ae70a2ab0f0e373
Koree <koreewilliam@gmail.com>
2014-03-12 20:44:52 -0400

    trying out distance

--- transit2/transit.js
+++ transit2/transit.js
@@ -4,8 +4,6 @@ var color;
 var bubble = new google.maps.InfoWindow();
 var marker;
 var m = [];
-var shortest;
-var shortname;
 
 
 function getMyLocation() {
@@ -67,7 +65,7 @@ var locations = [];
 
 function mapinfo(){
 
-
+var shortest = 4000;
 
 for (var i =0; i < 3; i++){
 
@@ -97,7 +95,7 @@ for (var i =0; i < 3; i++){
                     map: map,
                 });
             }
-           
+            var shortname;
            var length = distance(m[j]); //distance function
             if (length < shortest){
             shortest = length;
